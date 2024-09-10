c4c580552573f3873aecc740bc5f1be792119b12
Melissa Martinez <123403764+martinez415@users.noreply.github.com>
2024-09-10 02:16:10 +0000

    my dice rolls app

--- dice.rb
+++ dice.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "Hello World"
@@ -6,4 +13,60 @@ end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/home") do
+  erb(:elephant)
+end
+
+get("/dice/2/6") do
+  @rolls = []
+  2.times do
+    @die = rand(1..6)
+    @rolls.push(@die)
+  end
+	erb(:two_six)	
+end
+
+#GET /dice/2/10 (simulate two 10-sided dice)
+get("/dice/2/10") do
+  @rolls = []
+  2.times do
+    @die = rand(1..10)
+    @rolls.push(@die)
+  end
+  erb(:two_ten)
+end
+
+#GET /dice/1/20 (simulate one 20-sided die)
+get("/dice/1/20") do
+  @rolls = []
+  1.times do
+    @die = rand(1..20)
+    @rolls.push(@die)
+  end
+  erb(:one_twenty)
+end
+
+#GET /dice/5/4 (simulate five 4-sided dice)
+get("/dice/5/4") do
+  @rolls = []
+  5.times do
+    @die = rand(1..4)
+    @rolls.push(@die)
+  end
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+  100.times do
+    @first_die = rand(1..6)
+    @rolls.push(@first_die)
+  end
+  erb(:one_hundred_six)
 end
